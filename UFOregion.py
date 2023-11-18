b176954de312ba8961163d96af54eca9b73f65de
YangZH <108222683+RwandanMtGorilla@users.noreply.github.com>
2023-11-18 23:16:46 +0800

    Add files via upload

---
+++ UFOregion.py
@@ -0,0 +1,26 @@
+import csv
+from collections import Counter
+import matplotlib.pyplot as plt
+
+# 读取CSV文件并统计Region列
+country_counts = Counter()
+with open('ufo-sightings-transformed.csv', 'r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        country_counts[row['Region']] += 1
+
+# 按数量降序排序并取前30个
+sorted_countries = sorted(country_counts.items(), key=lambda x: x[1], reverse=True)[:30]
+
+# 分解为两个列表：国家和计数
+countries, counts = zip(*sorted_countries)
+
+# 绘制条形图
+plt.figure(figsize=(12, 8))  # 可以调整大小以更好地展示
+plt.bar(countries, counts)
+plt.xlabel('Country')
+plt.ylabel('Number of UFO Sightings')
+plt.title('Top 30 Regions with UFO Sightings')
+plt.xticks(rotation=45)
+plt.tight_layout()  # 调整布局以避免标签重叠
+plt.show()
